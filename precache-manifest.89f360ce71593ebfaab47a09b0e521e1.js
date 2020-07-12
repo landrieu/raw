b6a48cbdd5c5385ade8a5f75bddde27bfef6e0bd
Lionel Andrieu <lionel.andrieu@gmail.com>
2020-07-12 10:00:27 +1000

    Updates

---
+++ precache-manifest.89f360ce71593ebfaab47a09b0e521e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9b50f22f62dbc1d85c9993f67e16bd",
+    "url": "/raw/index.html"
+  },
+  {
+    "revision": "3593102807c9437ddd6b",
+    "url": "/raw/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "48dbc79338d396cb7ccd",
+    "url": "/raw/static/js/2.2c978956.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/raw/static/js/2.2c978956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3593102807c9437ddd6b",
+    "url": "/raw/static/js/main.fc44bc9e.chunk.js"
+  },
+  {
+    "revision": "9fd7ef3389b24d3c8043",
+    "url": "/raw/static/js/runtime-main.94f48573.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/raw/static/media/logo.5d5d9eef.svg"
+  }
+]);

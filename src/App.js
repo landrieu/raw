aad3e619a4a7077298a3abaed4338443a7926b31
Lionel Andrieu <lionel.andrieu@gmail.com>
2020-07-12 10:16:00 +1000

    Add Tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <h1 className="text-3xl text-blue-500">React App</h1>
     </div>
   );
 }
